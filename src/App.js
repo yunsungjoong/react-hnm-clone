cab8536682ef1a7a0fbbc6fc3da7f8157b27dc1a
yunsungjoong <yun_sj94@naver.com>
2024-04-06 14:15:02 +0900

    라우팅 세팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+/*
+1. 전체상품페이지, 로그인, 상품상세페이지
+2. 전체 상품페이지에서는 전체 상품을 볼 수 있다.
+3. 로그인 버튼 누르면 로그인 페이지가 나온다.
+4. 상품디테일을 눌렀으나, 로그인이 안되어 있을 경우에는 로그인 페이지가 먼저 나온다.
+5. 로그인이 되어 있을 경우 상품 디테일 페이지를 볼 수 있다.
+6. 로그아웃이 되면 상품 디테일페이지를 볼 수 없다. 다시 로그인 페이지가 보인다.
+7. 상품을 검색할 수 있다.
+*/
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
