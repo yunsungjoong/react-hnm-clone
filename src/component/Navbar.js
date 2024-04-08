571a9bcda2ba34d3b02030ebff949ed1490df4c4
yunsungjoong <yun_sj94@naver.com>
2024-04-08 22:55:36 +0900

    Search bar 및 메뉴 리스트 css 적용

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
 const Navbar = () => {
+    const menuList = [
+        "여성",
+        "Divided",
+        "남성",
+        "신생아/유아",
+        "아동",
+        "H&M Home",
+        "Sale",
+        "지속 가능성"
+    ];
     return (
         <div>
             <div>
-                <FontAwesomeIcon icon={faCoffee}/>
+                <div className="login-button">
+                    <FontAwesomeIcon icon={faUser} />
+                    <div>로그인</div>
+                </div>
             </div>
-            <div></div>
-            <div></div>
+            <div className="nav-logo">
+                <img width={130} src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/2560px-H%26M-Logo.svg.png" alt="HNM Logo"  />
+            </div>
+            <div className="menu-style">
+                <ul className="menu-list">
+                    {menuList.map((menu) => (
+                        <li className="menu-item">{menu}</li>
+                    ))}
+                </ul>
+                <div className="menu-search">
+                    <div className="input-container">
+                        <FontAwesomeIcon icon={faSearch} className='search-icon' />
+                        <input type="text" placeholder="Search" />
+                    </div>
+                </div>
+            </div>
             
         </div>
     );
