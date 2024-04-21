7bb0c02860baaed55af51c5a764026f4e095fdfb
yunsungjoong <yun_sj94@naver.com>
2024-04-21 18:31:50 +0900

    수정

--- src/route/PrivateRoute.js
+++ src/route/PrivateRoute.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ProductDetail from '../page/ProductDetail';
 import {Navigate} from "react-router-dom"
+
 const PrivateRoute = ( {authenticate} ) => {
     return authenticate === true? <ProductDetail /> : <Navigate to="/login/"/> 
 };
