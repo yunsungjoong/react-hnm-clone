c938a347b89aeb4d46e23ff76fd631ec7748bf6f
yunsungjoong <yun_sj94@naver.com>
2024-04-29 22:21:13 +0900

    login css 수정

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -3,7 +3,9 @@ import { useParams } from "react-router-dom"
 import {Container, Row, Col } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-
+import styled from "styled-components";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBagShopping, faCartShopping, faEnvelope, faHeart, faRuler } from '@fortawesome/free-solid-svg-icons';
 
 
 const ProductDetail = () => {
@@ -19,30 +21,53 @@ const ProductDetail = () => {
     useEffect(() => {
         getProductDetail()
     }, []);
+
+    
     return (
-        <Container>
+        <Container fluid="xxl" as="section" className="product-detail" >
             <Row>
-                <Col className="product-img"  sm={6}>
-                    <img src={product?.img} alt=''  />
+                <Col md="6" className="photo-img">
+                    <img src={product?.img} alt={product?.title}  />
                 </Col>
-                <Col sm={6} >
-                    <div>{product?.title}</div>
-                    <div>₩ {product?.price}원</div>
-                    <div></div>
+                <Col md="6" className="info" >
+                    <Title>
+                        <p>{product?.title}</p>
+                        <span className="heartIcon"><FontAwesomeIcon icon={faHeart}/></span>
+                    </Title>
+                    <Price>
+                        ₩ <span>{product?.price}</span>
+                    </Price>
+                    <p style={{marginBottom:"20px"}}>
+                        <Ment>{product?.choice ? "Conscious choice" : ""}</Ment>
+                        <Ment>{product?.new ? "신제품" : ""}</Ment>
+                    </p>
                     
-                    <div>
-                    <Form.Select className="mb-3">
-                        <option>사이즈 선택</option>
+                    <Form.Select aria-label="Default select example">
+                        <option disabled>사이즈 선택</option>
                         <option value="1">S</option>
                         <option value="2">M</option>
                         <option value="3">L</option>
                     </Form.Select>
-                    </div>
+                    <LinkBox>
+                        <p>
+                            <FontAwesomeIcon icon={faRuler} />
+                            <span>사이즈 가이드</span>
+                        </p>
+                        <p>
+                            <FontAwesomeIcon icon={faEnvelope} />
+                            <span>원하는 사이즈가 품절인가요?</span>
+                        </p>
+                    </LinkBox>
+                    
                     <div className="d-grid gap-2">
-                        <Button variant="danger" size="lg">
-                            구매하기
+                        <Button variant="danger">
+                            <FontAwesomeIcon icon={faCartShopping} />
+                            <span>바로 구매하기</span>
                         </Button>
-                        
+                        <Button variant="dark">
+                            <FontAwesomeIcon icon={faBagShopping} />
+                            <span>장바구니 추가</span>
+                        </Button>
                     </div>
                 </Col>
             </Row>
@@ -51,3 +76,53 @@ const ProductDetail = () => {
 };
 
 export default ProductDetail;
+
+const Title = styled.div`
+    display: flex;
+    justify-content: space-between;
+    font-size: clamp(1.2rem, 2vw, 1.7rem)
+    font-weight: 700;
+    margin-bottom: 20px;
+
+    .heartIcon{
+        cursor: pointer;
+    }
+`;
+
+const Price = styled.p`
+    margin-bottom: 18px;
+    font-size: clamp(1rem, 2vw, 1.4rem);
+    color: #333;
+    span {
+        font-weight: 600;
+    }
+`;
+
+const Ment = styled.span`
+    margin-right: 10px;
+    padding: 4px 10px;
+    border-raius: 4px;
+    background-color: var(--text-hlight);
+    color: #fff;
+`;
+
+const LinkBox = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-top: 18px;
+    margin-bottom: 40px;
+
+    p {
+        cursor: pointer;
+
+        span {
+            margin-left: 6px;
+            padding-bottom: 3px;
+            transition: 0.2s;
+        }
+
+        &:hover span{
+            border-bottom: 1px solid #666;
+        }
+    }
+`;
